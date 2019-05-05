003f99748b6bb9b3f8d499a53e9f013c9f25c2fc
KubaC123 <pikachucichy@gmail.com>
2019-05-05 14:33:30 +0200

    Initial Commit

---
+++ GraphAL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GraphAL
+//
+//  Created by Jakub Cichy on 05/05/2019.
+//  Copyright © 2019 Jakub Cichy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
